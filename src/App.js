b0b6562d8b73e3f96e8f80942a3b6385b3dc1fe9
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-12-19 12:08:30 +0530

    refactor: input and input check

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+let name = "";
+let year = "";
+
 export default function App() {
   const [out, setOut] = useState();
-  let name = "";
-  let year = "";
-  let flag = true;
-
-  function updateName(n) {
-    name = n.target.value;
-  }
-
-  function updateYear(y) {
-    year = y.target.value;
-  }
 
   function checkInput() {
     if (year < 0 || year > 2020 || isNaN(parseInt(year)) == isNaN(NaN))
-      flag = false;
+      return 0;
+    return 1;
   }
 
   function checkLeapYear() {
-    flag = true;
-    checkInput();
-    if (flag) {
+    if (checkInput()) {
       if (year % 400 == 0) return `${name} you were born in a Leap Year!!`;
       if (year % 100 == 0) return `${name} you were not born in a Leap Year`;
       if (year % 4 == 0) return `${name} you were born in a Leap Year!!`;
@@ -35,8 +26,14 @@ export default function App() {
   return (
     <div className="App">
       <h1>Check if your birth year is a leap year</h1>
-      <input placeholder="Enter your Name" onChange={updateName}></input>
-      <input placeholder="Enter your Birth Year" onChange={updateYear}></input>
+      <input
+        placeholder="Enter your Name"
+        onChange={(n) => (name = n.target.value)}
+      ></input>
+      <input
+        placeholder="Enter your Birth Year"
+        onChange={(y) => (year = y.target.value)}
+      ></input>
       <button onClick={() => setOut(checkLeapYear())}>Check</button>
       {out}
     </div>
